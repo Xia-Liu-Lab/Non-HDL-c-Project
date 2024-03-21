6a9da6a4c8af37180bfb467da2f2e36829e25d21
Shiyi0527 <162653305+Shiyi0527@users.noreply.github.com>
2024-03-21 16:19:22 +0800

    Add files via upload

---
+++ R code for selecting key metabolites based on linear regression analysis.R
@@ -0,0 +1,49 @@
+##linear regression
+# data matrix of metabolomics data
+cc <- read.csv()
+
+cc$sex<-factor(cc$sex, levels = c(0,1)) 
+cc$Smoking<-factor(cc$Smoking,levels = c(0,1))
+cc$Dringking<-factor(cc$Dringking,levels = c(0,1))
+cc$Hypertension<-factor(cc$Hypertension, levels = c(0,1))
+cc$Diabetes <-factor(cc$Diabetes , levels = c(0,1))
+cc$overweight<-factor(cc$overweight, levels = c(0,1))
+
+#Metabolites associated with Clostridium_sp_CAG_299 
+n <- c(12:ncol(cc))
+a <- NULL  #建一个空数据框
+for (i in n) {
+  lm.reg<-lm(Clostridium_sp_CAG_299~cc[,i]+age+sex+smoking+drinking+diet_diversity+physical_activity+overweight+diabetes+hypertension,data=cc)    
+  s<-summary(lm.reg)
+  r <- as.data.frame(s[["coefficients"]])[2,]
+  a <- rbind(a,r)
+}
+rownames(a)<-colnames(cc)[11:ncol(cc)]
+
+p <- data.frame(a)
+cc.fdr<- p[order(p[,4]),]
+FDR <- cc.fdr[,4]
+FDR <- data.frame(p.adjust(FDR,method = "BH"))
+colnames(FDR) <- "FDR"
+COU <- cbind(cc.fdr,FDR)
+write.csv(COU,file ="Clostridium_sp_CAG_299-FDR.CSV" ,row.names = T)
+
+
+#Metabolites associated with Eubacterium_rectale
+n <- c(12:ncol(cc))
+a <- NULL  #建一个空数据框
+for (i in n) {
+  lm.reg<-lm(Eubacterium_rectale~cc[,i]+age+sex+smoking+drinking+diet_diversity+physical_activity+overweight+diabetes+hypertension,data=cc)    
+  s<-summary(lm.reg)
+  r <- as.data.frame(s[["coefficients"]])[2,]
+  a <- rbind(a,r)
+}
+rownames(a)<-colnames(cc)[11:ncol(cc)]
+
+p <- data.frame(a)
+cc.fdr<- p[order(p[,4]),]
+FDR <- cc.fdr[,4]
+FDR <- data.frame(p.adjust(FDR,method = "BH"))
+colnames(FDR) <- "FDR"
+COU <- cbind(cc.fdr,FDR)
+write.csv(COU,file ="Eubacterium_rectale-FDR.CSV" ,row.names = T)
